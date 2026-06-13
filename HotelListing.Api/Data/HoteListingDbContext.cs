9467de8eb719b790d2f9a8372c867b4476814e43
OrdinaryUser9 <nijatrustamli2005@gmail.com>
2026-06-13 18:38:23 +0400

    - added entities and changed relationships (mainly)

---
+++ HotelListing.Api/Data/HoteListingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Api.Data;
+
+public class HoteListingDbContext : DbContext
+{
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Country> Countries { get; set; }
+
+    public HoteListingDbContext(DbContextOptions<HoteListingDbContext> options) : base(options)
+    {
+        
+    }
+}
