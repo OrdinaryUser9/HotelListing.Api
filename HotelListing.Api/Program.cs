791739682c29843017a91246069061ea40014ea4
OrdinaryUser9 <nijatrustamli2005@gmail.com>
2026-06-13 18:39:03 +0400

    Revert "- added entities and changed relationships (mainly)"
    This reverts commit 9467de8eb719b790d2f9a8372c867b4476814e43.

--- HotelListing.Api/Program.cs
+++ HotelListing.Api/Program.cs
@@ -1,11 +1,6 @@
-using HotelListing.Api.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
-builder.Services.AddDbContext<HoteListingDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
